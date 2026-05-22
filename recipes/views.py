2acb705cd1a8128da9a0c90d02a4d5ce7a513cc0
Leonardo <leo.sales.br@gmail.com>
2026-05-22 10:29:16 -0300

    Title

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'pages/home.html',)
+    return render(request, 'pages/home.html', context={'home': 'home',})
+
+def recipe(request, id):
+    return render(request, 'pages/recipe_view.html', context={'id': id,})
